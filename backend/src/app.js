c561913351fb6c30a1debc374a5801fea66d6ef9
Teo Goerens <goerens_teo@hotmail.com>
2024-03-10 19:03:02 -0300

    acomodo el limite de entity too large

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ dotenv.config();
 import cors from "cors";
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json({ limit: "8mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 import { fileURLToPath } from "url";
