d0a708e873528e9c88dfe4dbf7f69b5b2c5bbf82
Teo Goerens <goerens_teo@hotmail.com>
2024-04-06 23:51:56 -0300

    cambios en el base url

--- frontend/src/components/Layout/Buttons/ToggleOpenForVote.jsx
+++ frontend/src/components/Layout/Buttons/ToggleOpenForVote.jsx
@@ -1,15 +1,45 @@
 //Import React & Hooks
-import React from "react";
+import React, { useState } from "react";
 
 //Import CSS & styles
 import "./ButtonsStyle.css";
 
 const ToggleOpenForVote = ({ isOpen, onClick, id }) => {
+  const [confirmationOpen, setConfirmationOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setConfirmationOpen(true);
+  };
+
+  const handleConfirmToggle = () => {
+    onClick(id);
+    setConfirmationOpen(false);
+  };
+
+  const handleCancelToggle = () => {
+    setConfirmationOpen(false);
+  };
+
   const buttonClassName = isOpen ? "close-vote" : "open-vote";
   return (
-    <button className={buttonClassName} onClick={() => onClick(id)}>
-      {isOpen ? "Close" : "Open"}
-    </button>
+    <>
+      <button className={buttonClassName} onClick={handleOpenModal}>
+        {isOpen ? "Close" : "Open"}
+      </button>
+
+      {confirmationOpen && (
+        <div className="delete-confirmation-overlay">
+          <div className="delete-confirmation-modal">
+            <h4>¿Estás seguro de que querés cambiar el estado de la fecha?</h4>
+            <p>Se informará a todos los usuarios registrados vía mail</p>
+            <div className="delete-confirmation-btn-container">
+              <button onClick={handleConfirmToggle}>Si</button>
+              <button onClick={handleCancelToggle}>Cancelar</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
