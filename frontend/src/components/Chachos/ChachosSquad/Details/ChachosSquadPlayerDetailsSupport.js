55d069c94e55297af4e446449053d1cfbaecdd16
Teo Goerens <goerens_teo@hotmail.com>
2024-04-10 21:19:47 -0300

    foto pincho

--- frontend/src/components/Chachos/ChachosSquad/Details/ChachosSquadPlayerDetailsSupport.js
+++ frontend/src/components/Chachos/ChachosSquad/Details/ChachosSquadPlayerDetailsSupport.js
@@ -2,6 +2,7 @@ import player10ImageSource from "../../../../assets/photos/players/player-10.jpe
 import player2ImageSource from "../../../../assets/photos/players/player-2.jpeg";
 import player23ImageSource from "../../../../assets/photos/players/player-23.jpeg";
 import player14ImageSource from "../../../../assets/photos/players/player-14.jpeg";
+import player32ImageSource from "../../../../assets/photos/players/player-32.jpeg";
 
 const chachosSquadImages = [
   {
@@ -19,6 +20,10 @@ const chachosSquadImages = [
   {
     img: player14ImageSource,
     shirt: 14,
+  },
+  {
+    img: player32ImageSource,
+    shirt: 32,
   },
 ];
 
