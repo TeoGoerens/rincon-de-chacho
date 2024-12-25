56d5c45a6a7c28ed16173526e2fea6a92e69d171
Teo Goerens <goerens_teo@hotmail.com>
2024-12-25 15:34:02 -0300

    cambios en el mail de cronica nueva

--- frontend/src/helpers/baseURL.js
+++ frontend/src/helpers/baseURL.js
@@ -1,3 +1,6 @@
 export const baseURL = process.env.REACT_APP_BASE_URL || "";
 
-//http://localhost:8080
+// http://localhost:8080
+// Esta es la dirección del localhost que se utiliza en desarrollo y cuya información está almacenada en el archivo .env
+// En producción, dado que no definí un archivo .env, al no encontrar la variable REACT_APP_BASE_URL directamente trae un string vacío
+// Como el frontend y el backend están servidos en el mismo dominio, entonces el string vacío es correcto para manejo de rutas relativas
